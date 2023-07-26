0e8272562ea25c4d31a2e22130e6f9c3fb0dc157
JoeWebDev70 <joellemoureu@gmail.com>
2023-07-26 11:39:48 +0200

    chore: Chores
    preparing project : readme, webpack installation and configuration of plugin for js, sass, ... minimizer

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 06-lab-js-todo
+
+CONSIGNE
+
+Votre défi consiste à créer cette application de gestionnaire de tâche et à la rendre aussi proche que possible de la conception. Vous devez réaliser la maquette d’après les tendances graphiques du moment de votre composant de votre composant pour le desktop mais aussi pour la version mobile.
+Intégrer votre composant et ses interactions avec l’utilisateur.
+
+Vos utilisateurs doivent pouvoir :
+- Rechercher des mots à l'aide du champ de saisie.
+- Ajouter de nouvelles tâches à la liste.
+- Marquer les tâches comme terminées.
+- Supprimer les tâches de la liste.
+- Filtrer par toutes les tâches/actives/complètes.
+- Effacer toutes les tâches terminées.
+- Basculer entre les modes clair et sombre.
+- Glisser déposer pour réorganiser les éléments de la liste.
+- Gardez une trace de toutes les modifications, même après avoir actualisé le navigateur (localstorage)
+- Voir la disposition optimale de l'interface en fonction de la taille de l'écran de leur appareil.
+- Voir les états de survol et de focus pour tous les éléments interactifs de la page.
+
+Technologies
+- Écrivez vos styles à l'aide du pré processeur Sass
+- Utiliser webpack et babel
+- Utiliser un system de Kanban pour lister vos tâches et suivre vos tâches.
+- Tout doit être généré via JS dans le body seule une balise qui servira de containeur de votre composant est autorisée.
+- Vous devez utiliser le mécanisme Template Content. Vous trouverez la documentation dans le lien ci dessous :
+https://developer.mozilla.org/fr/docs/Web/HTML/Element/template
+
+Gestion de projet:
+- Essayez d'estimer le temps qu'il vous faudra pour construire le projet. Ensuite, voyez si le temps pris correspond à votre estimation. Les estimations de projet sont une compétence souvent négligée, mais importante pour les développeurs professionnels.
+- Envoyer à votre formateur la date de fin de rendu du projet en indiquant le nom du projet.
+
+Production attendue
+- Page HTML
+- Page CSS
+- Page JS
+- Les médias
+- Figma
+
+Link to Project : <a href="https://github.com/JoeWebDev70/06-lab-js-todo_project"> 06-lab-js-todo_project</a>
---
+++ webpack.config.js
@@ -0,0 +1,134 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
+const isProduction = process.env.NODE_ENV == 'production';
+const stylesHandler = MiniCssExtractPlugin.loader;
+
+const config = {
+    entry: {
+        script: './src/script.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: "script-build.js",
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+        liveReload: true,
+        port: 9000,
+        static: {
+            directory: path.join(__dirname, "public"),
+        },
+        hot: false,
+        https: false,
+        client: {
+            progress: true,
+            overlay: {
+                errors : true,
+                warnings : false,
+                runtimeErrors : true
+            }
+        },
+    },
+    optimization: {
+        minimizer: [
+          new CssMinimizerPlugin(),
+          new TerserPlugin({
+            test: /\.js$/i, 
+            exclude: /node_modules/,
+          }),
+          new ImageMinimizerPlugin({
+            generator: [
+              {
+                preset: "webp",
+                implementation: ImageMinimizerPlugin.sharpGenerate,
+                options: {
+                  encodeOptions:{
+                    webp: {
+                      quality: 60,
+                    }
+                  }
+                }
+              }
+            ],
+          })
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject : true,
+            minify : true,
+            template: "./src/index.html",
+            filename: "index.html",
+            
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: "src/ressources", to: "ressources"},
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: [
+                        ['@babel/preset-env', { targets: {"ie" : "11"} }]
+                    ]
+                    },
+                },
+            },
+            {
+                test: /\.html?$/,
+                use: [
+                  {
+                    loader : "html-loader",
+                    options : {minimize: true}
+                  }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [stylesHandler,'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(eot|ttf|woff|woff2)$/i,
+                type: 'asset',
+                generator:{
+                    filename:'./ressources/fonts/[name][ext]',
+                }, 
+            },
+            {
+                test: /\.(webp|svg|png|jpe?g|gif)$/i,
+                type: 'asset',
+                generator:{
+                    filename:'./ressources/images/[name][ext]',
+                }, 
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
